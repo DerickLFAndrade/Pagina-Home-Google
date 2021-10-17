84d46b837ef614f6bb32ede27f23b6ecb7b2b7d2
DerickLFAndrade <derickspy15@gmail.com>
2021-10-17 02:35:29 -0300

    hover dos links

--- javaScript.js
+++ javaScript.js
@@ -10,7 +10,8 @@ var botPesq = document.getElementById('botPesq')
 var botSorte = document.getElementById('botSorte')
 var boxText = document.createElement('div')
 var boxText2 = document.createElement('div')
-var contTec = document.getElementById('contTec')
+var contCaixas = document.getElementById('contCaixas')
+var contCaixas2 = document.getElementById('contCaixas2')
 search.focus()
 var a = document.getElementById('pesquisar');
 
@@ -37,44 +38,17 @@ botX.onclick = function () {
 
 
 icoTec.onmouseover = function () {
-    boxText.innerHTML= 'Ferramenta de inserção de texto'
-    boxText.style.background = 'rgb(53, 52, 52)'
-    boxText.style.borderRadius = '1px'
-    boxText.style.height = '22px'
-    boxText.style.width = '200px'
-    boxText.style.textIndent = '10px'
-    boxText.style.color = 'white'
-    boxText.style.marginLeft = '-200px'
-    boxText.style.fontSize = '12px'
-    boxText.style.fontWeight = 'bold'
-    
-    boxText.style.margin = '5px'
-    
-    contTec.appendChild(boxText)
+  contCaixas.style.display = 'block'
 }
 icoTec.onmouseleave = function () {
-    boxText.remove(boxText)
+    contCaixas.style.display = 'none'
 }
 
 icoMic.onmouseover = function () {
-    boxText2.innerHTML = 'Pesquisa por voz'
-    boxText2.style.background = 'rgb(53, 52, 52)'
-    boxText2.style.borderRadius = '1px'
-    boxText2.style.height = '23px'
-    boxText2.style.width = '100px'
-    boxText2.style.textIndent = '15px'
-    boxText2.style.color = 'white'
-    boxText2.style.marginLeft = '-200px'
-    boxText2.style.marginTop = '100px'
-    boxText2.style.fontSize = '12px'
-    boxText2.style.fontWeight = 'bold'
-
-    boxText2.style.margin = '5px'
-
-    contTec.appendChild(boxText2)
+    contCaixas2.style.display = 'block'
 }
 icoMic.onmouseleave = function () {
-    boxText2.remove(pesquisaG)
+    contCaixas2.style.display = 'none'
 }
 
 
